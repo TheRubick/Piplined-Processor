a997af469fe37a810b4734f0f9b9114a389f080a
Gellesh <khaloudfire104@gamil.com>
2020-05-06 22:18:14 +0200

    Merge Exec and Mem stage

--- Memory Stage/execute_mem_stages.vhd
+++ Memory Stage/execute_mem_stages.vhd
@@ -5,7 +5,6 @@ use IEEE.std_logic_unsigned.all;
 
 ENTITY decode_buffer IS
 PORT(
-
   CLK ,RST       :  IN std_logic;
   IR_out: in std_logic_vector (15 downto 0);
   RET_out: in std_logic;
@@ -33,8 +32,7 @@ PORT(
 );
 END decode_buffer;
 
-
-architecture decode_buffer of decode_buffer_arch is
+architecture decode_buffer_arch of decode_buffer is
 
 component EX_STAGE IS
 PORT(
@@ -53,7 +51,7 @@ Predication, Predication_Done, Flush_out :  OUT std_logic; -- BranchPredicator o
 DP1_EX, DP2_EX : OUT std_logic;
 PC_EX, ADD_DST1_EX, DATA_DST2_EX : OUT std_logic_vector(31 downto 0)
 );
-END component; 
+END component;
 
 
 
@@ -71,13 +69,25 @@ CALL_OUT, RET_OUT, RTI_OUT, OUT_OUT, IN_OUT, INT_OUT ,INC_OUT, DEC_OUT, DP1_OUT,
 ) ;
 end component;
 
+-- signals outed from Execute stage to Execute - Memory buffer
 signal Predication, Predication_Done, Flush_out :  std_logic; -- BranchPredicator outputs
 signal DP1_EX, DP2_EX : std_logic;
 signal PC_EX, ADD_DST1_EX, DATA_DST2_EX : std_logic_vector(31 downto 0);
+------------------------------------------------------------------------
+-- signals outed from Execute buffer to Memory Stage
+signal PC_OUT, ADD_DST1_OUT, DATA_DST2_OUT :  std_logic_vector(31 downto 0);
+signal reg1_wr_OUT, reg2_wr_OUT, dst1_add_OUT, dst2_add_OUT, MEM_WR_OUT, MEM_RD_OUT  : std_logic;
+signal CALL_OUT, RET_OUT, RTI_OUT, OUT_OUT, IN_OUT, INT_OUT ,INC_OUT, DEC_OUT, DP1_OUT, DP2_OUT : std_logic
+------------------------------------------------------------------------
+SIGNAL Notfound : std_logic_vector(31 downto 0):= (others => '0');
 
 begin
-	
-	execute_component : EX_STAGE port map(CLK,RST,IR_out,INT_out,JZ_out,'0',);
+  -- connect Execute Stage
+execute_component : EX_STAGE port map(CLK, RST, IR_out, INT_out, JZ_out, '0', '0', '0', '0', '0', '0', '0', '0', OUT1_out, OUT2_out, Notfound, Notfound, Notfound, Notfound, PC_IF_EX_out, Notfound, ALU_ENABLE_out, IMM_out, EA_out, Predication,  Predication_Done,  Flush_out, DP1_EX,  DP2_EX, PC_EX, ADD_DST1_EX,  DATA_DST2_EX);
+
+-- Execute Memory Buffer
+EX_MEM_Buffer_component : EX_MEM_Buffer port map (CLK, RST, '0', PC_EX, ADD_DST1_EX, DATA_DST2_EX, PC_OUT, ADD_DST1_OUT, DATA_DST2_OUT, REG1_WR_out, REG2_WR_out, DST1_ADD_out, DST2_ADD_out, reg1_wr_OUT, reg2_wr_OUT, dst1_add_OUT, dst2_add_OUT, MEM_WR_OUT, MEM_RD_OUT, CALL_out, RET_out, RTI_out, OUT_SIGNAL_out,'0',INT_out,'0','0',DP1_EX, DP2_EX, CALL_OUT, RET_OUT, RTI_OUT, OUT_OUT, IN_OUT, INT_OUT ,INC_OUT, DEC_OUT, DP1_OUT, DP2_OUT);
+
+-- notes  - all dp loadcase is missing and inc , dec , in signals and Dst1_EX, Dst1_MEM, Dst2_EX, Dst2_MEM,  JMP_INT_PC are also missing
 
 end decode_buffer_arch;
-
